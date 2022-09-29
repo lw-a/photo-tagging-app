472437e9a58a734e6817430687b90f333d22f733
Adam Hemming <hemming.dev@gmail.com>
2022-09-29 19:57:32 +0900

    style nav

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import level1 from './images/level1.png'
 import './styles/App.css';
 
 function App() {
+
+  const pokemon = {
+    levelOne: {
+      first: {
+        name: 'Weedle',
+        sprite: 'https://img.pokemondb.net/sprites/silver/normal/weedle.png'
+      },
+      second: {
+        name: 'Slowpoke',
+        sprite: 'https://img.pokemondb.net/sprites/ruby-sapphire/normal/slowpoke.png'
+      },
+      third: {
+        name: 'Caterpie',
+        sprite: 'https://img.pokemondb.net/sprites/silver/normal/caterpie.png'
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <Nav />
+      <Nav pokemon={pokemon.levelOne}/>
       <div className='container-fluid'>
         <img src={level1} alt="game" className="img-fluid w-100"/>
       </div>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,18 @@
 import '../styles/Nav.css';
 
-function Nav() {
+function Nav(props) {
+
+  const pokemon = props.pokemon
+
   return(
-    <nav>nav</nav>
+    <nav className='navbar d-flex justify-content-center'>
+      <h2 className="navbar-brand">Find the 3 Pokemon!</h2>
+      <ul className="list-group list-group-horizontal list-group-flush">
+        <li className="list-group-item border-0">{pokemon.first.name} <img src={pokemon.first.sprite} alt =''></img> </li>
+        <li className="list-group-item border-0">{pokemon.second.name} <img src={pokemon.second.sprite} alt =''></img></li>
+        <li className="list-group-item border-0">{pokemon.third.name} <img src={pokemon.third.sprite} alt =''></img></li>
+      </ul>
+    </nav>
   )
 }
 

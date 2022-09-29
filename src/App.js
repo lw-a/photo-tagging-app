3d449d01bb416d4b79e736805bdd149f84280343
Adam Hemming <hemming.dev@gmail.com>
2022-09-29 17:21:39 +0900

    add image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import level1 from './images/level1.png'
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">Hello from App!</div>
+    <div className="App">
+      <div className='container-fluid'>
+        <img src={level1} alt="game" className="img-fluid w-100"/>
+      </div>
+    </div>
+
   );
 }
 

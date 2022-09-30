5f0fbd8076ff88ac18f6d7507311554b388752ad
Adam Hemming <hemming.dev@gmail.com>
2022-09-30 20:00:34 +0900

    More modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Nav from "./Components/Nav";
 import Game from "./Components/Game";
+import StartModal from "./Components/StartModal";
 import levelOne from './images/levelOne.png'
 import { useState } from "react";
 import './styles/App.css';
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
 
   const [background, setBackground] = useState(levelOne)
+
+  function displayModal() {
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
 
   const pokemon = {
     levelOne: {
@@ -31,6 +41,7 @@ function App() {
       <div className='container-fluid'>
         <Game background={background} />
       </div>
+      <StartModal show={showModal} />
     </div>
 
   );
--- src/Components/StartModal.js
+++ src/Components/StartModal.js
@@ -1,21 +1,55 @@
-function StartModal(props) {
-  return(
-<div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Pokemon Hunt!</h5>
-      </div>
-      <div class="modal-body">
-        <p>Try and find all 3 pokemon as fast as possible!</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Let's Go!</button>
-      </div>
-    </div>
-  </div>
-</div>
-  )
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function StartModal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
+
+// function StartModal(props) {
+//   return(
+// <div className={props.show ? "modal show" : "modal"}>
+//   <div className="modal-dialog">
+//     <div className="modal-content">
+//       <div className="modal-header">
+//         <h5 className="modal-title">Pokemon Hunt!</h5>
+//       </div>
+//       <div className="modal-body">
+//         <p>Try and find all 3 pokemon as fast as possible!</p>
+//       </div>
+//       <div className="modal-footer">
+//         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Let's Go!</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//   )
+// }
 
 export default StartModal;

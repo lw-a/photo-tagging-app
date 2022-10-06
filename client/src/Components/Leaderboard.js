a97dbe49a9314e5d3e438fdaf2148e27cd36b483
Adam Hemming <hemming.dev@gmail.com>
2022-10-06 22:21:05 +0900

    working on leaderboard

--- client/src/Components/Leaderboard.js
+++ client/src/Components/Leaderboard.js
@@ -1,71 +1,77 @@
-// import Nav from "./Nav";
-// import React, { useEffect, useState } from 'react';
+import Nav from "./Nav";
+import React, { useEffect, useState } from 'react';
 
-// function Leaderboard() {
+function Leaderboard() {
 
-//   useEffect(() => {
-//     window.fetch('http://localhost:3001/api/scores')
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-//     .catch(error => console.log(error));
-//   })
+  const [scores, setScores] = useState([]);
 
-//   return (
-//     <div>
-//       <Nav />
-//       <div className="container">
-//         <ul className="nav nav-tabs">
-//           <li className="nav-item">
-//             <a className="nav-link active" aria-current="page" href="#">Viridan Forest</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">Mt. Moon</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">Cerulean City</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">Celadon City Fair</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">Safari Zone</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">Indigo Plateau</a>
-//           </li>
-//         </ul>
-//         <div>
-
-//         </div>
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default Leaderboard;
-
-import React, { Component } from 'react';
-
-class Leaderboard extends Component {
-  componentDidMount() {
+  useEffect(() => {
     window.fetch('/api/scores')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => setScores(json))
       .catch(error => console.log(error));
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+  }, []);
+
+  console.log(scores)
+  return (
+    <div>
+      <Nav />
+      <div className="container">
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Viridan Forest</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Mt. Moon</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Cerulean City</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Celadon City Fair</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Safari Zone</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Indigo Plateau</a>
+          </li>
+        </ul>
+        <div>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {scores.map(score => <div>{score}</div>)}
       </div>
-    );
-  }
+
+    </div>
+  )
 }
 
 export default Leaderboard;
+
+// import React, { Component } from 'react';
+
+// class Leaderboard extends Component {
+
+//   componentDidMount() {
+//     window.fetch('/api/scores')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+//       .catch(error => console.log(error));
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Leaderboard;
